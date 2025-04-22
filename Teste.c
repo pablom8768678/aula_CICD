4aa6872aaca6e500f1ff32ace1ee697f4ddeffc9
pablom8768678 <122410885@ulife.com.br>
2025-04-22 20:35:20 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -3,8 +3,7 @@
 
 void somar(){
   //Função de somar simples
-  assert(1 + 1 == 2); // Verifica se 1 é igual a 1
-  printf("Somando: 1 + 1 = \n" + 1+1);
+  printf("A soma é: %d\n", 1 + 1);
 }
 
 int main(){
