30b83a20089cb6abc7c9d7ed4750c5227792e18d
pablom8768678 <122410885@ulife.com.br>
2025-04-22 20:32:36 -0300

    Update Teste.c

--- Teste.c
+++ Teste.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <assert.h>
 
-void testar(){
-  //Função de teste simples
-  assert(1 == 1); // Verifica se 1 é igual a 1
-  printf("Teste passou!\n");
+void somar(){
+  //Função de somar simples
+  assert(1 + 1 == 2); // Verifica se 1 é igual a 1
+  printf("Somando: 1 + 1 = \n" + 1+1);
 }
 
 int main(){
-  testar();
+  somar();
   return 0;
 }
